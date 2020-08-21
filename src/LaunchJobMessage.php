17cf4e3b5fc50a2d9f4e1297ce93f44ceea47c38
Yann Eugoné <yeugone@prestaconcept.net>
2020-08-21 18:42:31 +0200

    Switch to monorepo

---
+++ src/LaunchJobMessage.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Yokai\Batch\Bridge\Symfony\Messenger;
+
+final class LaunchJobMessage
+{
+    /**
+     * @var string
+     */
+    private $jobName;
+
+    /**
+     * @var array
+     */
+    private $configuration;
+
+    public function __construct(string $jobName, array $configuration = [])
+    {
+        $this->jobName = $jobName;
+        $this->configuration = $configuration;
+    }
+
+    public function getJobName(): string
+    {
+        return $this->jobName;
+    }
+
+    public function getConfiguration(): array
+    {
+        return $this->configuration;
+    }
+}
